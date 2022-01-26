f2644bbf203e9cc41dc687433a0ecf2f42f3f43a
LauZanMo <LauZanMo@whu.edu.cn>
2022-01-26 22:27:17 +0800

    修改以兼容acfly的setpoint_raw插件

--- mavros/src/plugins/setpoint_raw.cpp
+++ mavros/src/plugins/setpoint_raw.cpp
@@ -1,17 +1,15 @@
 /**
- * @brief SetpointRAW plugin
  * @file setpoint_raw.cpp
+ * @author LauZanMo (LauZanMo@whu.edu.cn)
  * @author Vladimir Ermakov <vooon341@gmail.com>
+ * @brief This file is from mavros open source respository, thanks for their contribution.
+ * @version 1.0
+ * @date 2022-01-26
  *
- * @addtogroup plugin
- * @{
- */
-/*
- * Copyright 2015,2016 Vladimir Ermakov.
+ * @copyright Copyright (c) 2022 acfly
+ * @copyright Copyright 2014,2015,2016,2017 Vladimir Ermakov.
+ * For commercial use, please contact acfly: https://www.acfly.cn
  *
- * This file is part of the mavros package and subject to the license terms
- * in the top-level LICENSE file of the mavros repository.
- * https://github.com/mavlink/mavros/tree/master/LICENSE.md
  */
 
 #include <eigen_conversions/eigen_msg.h>
@@ -26,21 +24,26 @@ namespace mavros {
 namespace std_plugins {
 /**
  * @brief Setpoint RAW plugin
- *
- * Send position setpoints and publish current state (return loop).
+ * @note Send position setpoints and publish current state (return loop).
  * User can decide what set of filed needed for operation via IGNORE bits.
+ * @brief offboard模式飞控控制ROS插件，使用Setpoint RAW信息
+ * @note 分别发送SET_ATTITUDE_TARGET，SET_POSITION_TARGET_LOCAL_NED和
+ * SET_POSITION_TARGET_GLOBAL_INT信息并对相应应答信息进行监控。
+ * @warning 该插件依赖于sys_time插件提供time_offset用于消除飞控与机载电脑的时钟偏移
+ * @warning 该插件依赖于系统兼容性支持
  */
 class SetpointRawPlugin : public plugin::PluginBase,
                           private plugin::SetPositionTargetLocalNEDMixin<SetpointRawPlugin>,
                           private plugin::SetPositionTargetGlobalIntMixin<SetpointRawPlugin>,
                           private plugin::SetAttitudeTargetMixin<SetpointRawPlugin> {
 public:
-    SetpointRawPlugin() : PluginBase(), sp_nh("~setpoint_raw") {}
+    SetpointRawPlugin()
+        : PluginBase(), sp_nh("~setpoint_raw"), set_attitude_support_confirmed(false),
+          set_position_local_support_confirmed(false),
+          set_position_global_support_confirmed(false) {}
 
     void initialize(UAS &uas_) override {
         PluginBase::initialize(uas_);
-
-        bool tf_listen;
 
         local_sub         = sp_nh.subscribe("local", 10, &SetpointRawPlugin::local_cb, this);
         global_sub        = sp_nh.subscribe("global", 10, &SetpointRawPlugin::global_cb, this);
@@ -49,13 +52,15 @@ public:
         target_global_pub = sp_nh.advertise<mavros_msgs::GlobalPositionTarget>("target_global", 10);
         target_attitude_pub = sp_nh.advertise<mavros_msgs::AttitudeTarget>("target_attitude", 10);
 
-        // Set Thrust scaling in px4_config.yaml, setpoint_raw block.
+        // Set Thrust scaling in config.yaml, otherwise set attitude will be ignored.
+        // 在配置文件中设置拉力的比例，否则设置角度的命令会被忽略
         if (!sp_nh.getParam("thrust_scaling", thrust_scaling)) {
-            ROS_WARN_THROTTLE_NAMED(5, "setpoint_raw",
-                                    "thrust_scaling parameter is unset. Attitude (and angular "
-                                    "rate/thrust) setpoints will be ignored.");
+            ROS_WARN_NAMED("setpoint_raw", "SPR: thrust_scaling parameter is unset. Attitude (and "
+                                           "angular rate/thrust) setpoints will be ignored.");
             thrust_scaling = -1.0;
         }
+
+        enable_capabilities_cb();
     }
 
     Subscriptions get_subscriptions() override {
@@ -67,6 +72,9 @@ public:
     }
 
 private:
+    using lock_guard = std::lock_guard<std::recursive_mutex>;
+    std::recursive_mutex mutex;
+
     friend class SetPositionTargetLocalNEDMixin;
     friend class SetPositionTargetGlobalIntMixin;
     friend class SetAttitudeTargetMixin;
@@ -76,16 +84,23 @@ private:
     ros::Publisher  target_local_pub, target_global_pub, target_attitude_pub;
 
     double thrust_scaling;
+    bool   set_attitude_support_confirmed;
+    bool   set_position_local_support_confirmed;
+    bool   set_position_global_support_confirmed;
 
-    /* -*- message handlers -*- */
-    void handle_position_target_local_ned(const mavlink::mavlink_message_t *               msg,
+    /* message handlers */
+    /* 信息回调句柄 */
+
+    //! @warning: 下面的坐标系原来的代码坐标系转换好像和实际不符，需要测试
+    void handle_position_target_local_ned(const mavlink::mavlink_message_t                *msg,
                                           mavlink::common::msg::POSITION_TARGET_LOCAL_NED &tgt) {
-        // Transform desired position,velocities,and accels from ENU to NED frame
+        // Transform desired position,velocities,and accels from NED to ENU frame
+        // 将期望的位置、速度和加速度从NED转到ENU
         auto  position = ftf::transform_frame_ned_enu(Eigen::Vector3d(tgt.x, tgt.y, tgt.z));
         auto  velocity = ftf::transform_frame_ned_enu(Eigen::Vector3d(tgt.vx, tgt.vy, tgt.vz));
         auto  af       = ftf::transform_frame_ned_enu(Eigen::Vector3d(tgt.afx, tgt.afy, tgt.afz));
         float yaw      = ftf::quaternion_get_yaw(ftf::transform_orientation_aircraft_baselink(
-            ftf::transform_orientation_ned_enu(ftf::quaternion_from_rpy(0.0, 0.0, tgt.yaw))));
+                 ftf::transform_orientation_ned_enu(ftf::quaternion_from_rpy(0.0, 0.0, tgt.yaw))));
         Eigen::Vector3d ang_vel_ned(0.0, 0.0, tgt.yaw_rate);
         auto            ang_vel_enu = ftf::transform_frame_ned_enu(ang_vel_ned);
         float           yaw_rate    = ang_vel_enu.z();
@@ -104,13 +119,14 @@ private:
         target_local_pub.publish(target);
     }
 
-    void handle_position_target_global_int(const mavlink::mavlink_message_t *                msg,
+    void handle_position_target_global_int(const mavlink::mavlink_message_t                 *msg,
                                            mavlink::common::msg::POSITION_TARGET_GLOBAL_INT &tgt) {
-        // Transform desired velocities from ENU to NED frame
+        // Transform desired velocities from NED to ENU frame
+        // 将期望的速度从NED转到ENU
         auto  velocity = ftf::transform_frame_ned_enu(Eigen::Vector3d(tgt.vx, tgt.vy, tgt.vz));
         auto  af       = ftf::transform_frame_ned_enu(Eigen::Vector3d(tgt.afx, tgt.afy, tgt.afz));
         float yaw      = ftf::quaternion_get_yaw(ftf::transform_orientation_aircraft_baselink(
-            ftf::transform_orientation_ned_enu(ftf::quaternion_from_rpy(0.0, 0.0, tgt.yaw))));
+                 ftf::transform_orientation_ned_enu(ftf::quaternion_from_rpy(0.0, 0.0, tgt.yaw))));
         Eigen::Vector3d ang_vel_ned(0.0, 0.0, tgt.yaw_rate);
         auto            ang_vel_enu = ftf::transform_frame_ned_enu(ang_vel_ned);
         float           yaw_rate    = ang_vel_enu.z();
@@ -131,14 +147,15 @@ private:
         target_global_pub.publish(target);
     }
 
-    void handle_attitude_target(const mavlink::mavlink_message_t *     msg,
+    void handle_attitude_target(const mavlink::mavlink_message_t      *msg,
                                 mavlink::common::msg::ATTITUDE_TARGET &tgt) {
-        // Transform orientation from baselink -> ENU
-        // to aircraft -> NED
+        // Transform orientation from aircraft -> NED
+        // to baselink -> ENU
+        // 将期望的旋转从FRD->NED转到FLU->ENU
         auto orientation = ftf::transform_orientation_ned_enu(
-            ftf::transform_orientation_baselink_aircraft(ftf::mavlink_to_quaternion(tgt.q)));
+            ftf::transform_orientation_aircraft_baselink(ftf::mavlink_to_quaternion(tgt.q)));
 
-        auto body_rate = ftf::transform_frame_baselink_aircraft(
+        auto body_rate = ftf::transform_frame_aircraft_baselink(
             Eigen::Vector3d(tgt.body_roll_rate, tgt.body_pitch_rate, tgt.body_yaw_rate));
 
         auto target = boost::make_shared<mavros_msgs::AttitudeTarget>();
@@ -152,100 +169,155 @@ private:
         target_attitude_pub.publish(target);
     }
 
-    /* -*- callbacks -*- */
+    /* mid-level functions */
+    /* 中间件函数 */
 
-    void local_cb(const mavros_msgs::PositionTarget::ConstPtr &req) {
-        Eigen::Vector3d position, velocity, af;
-        float           yaw, yaw_rate;
-
-        tf::pointMsgToEigen(req->position, position);
-        tf::vectorMsgToEigen(req->velocity, velocity);
-        tf::vectorMsgToEigen(req->acceleration_or_force, af);
-
-        // Transform frame ENU->NED
-        if (req->coordinate_frame == mavros_msgs::PositionTarget::FRAME_BODY_NED ||
-            req->coordinate_frame == mavros_msgs::PositionTarget::FRAME_BODY_OFFSET_NED) {
-            position = ftf::transform_frame_baselink_aircraft(position);
-            velocity = ftf::transform_frame_baselink_aircraft(velocity);
-            af       = ftf::transform_frame_baselink_aircraft(af);
-            yaw =
-                ftf::quaternion_get_yaw(ftf::transform_orientation_absolute_frame_aircraft_baselink(
-                    ftf::quaternion_from_rpy(0.0, 0.0, req->yaw)));
+    // Acts when capabilities of the fcu are changed
+    // 当飞控兼容性改变时执行该函数
+    void capabilities_cb(UAS::MAV_CAP capabilities) override {
+        lock_guard lock(mutex);
+        if (m_uas->has_capability(UAS::MAV_CAP::SET_ATTITUDE_TARGET)) {
+            set_attitude_support_confirmed = true;
+            ROS_INFO_NAMED("setpoint_raw", "SPR: Set attitude target command is supported.");
         } else {
-            position = ftf::transform_frame_enu_ned(position);
-            velocity = ftf::transform_frame_enu_ned(velocity);
-            af       = ftf::transform_frame_enu_ned(af);
-            yaw      = ftf::quaternion_get_yaw(ftf::transform_orientation_aircraft_baselink(
-                ftf::transform_orientation_ned_enu(ftf::quaternion_from_rpy(0.0, 0.0, req->yaw))));
+            set_attitude_support_confirmed = false;
+            ROS_WARN_NAMED("setpoint_raw", "SPR: Set attitude target command is not supported.");
         }
 
-        Eigen::Vector3d ang_vel_enu(0.0, 0.0, req->yaw_rate);
-        auto            ang_vel_ned = ftf::transform_frame_ned_enu(ang_vel_enu);
-        yaw_rate                    = ang_vel_ned.z();
+        if (m_uas->has_capability(UAS::MAV_CAP::SET_POSITION_TARGET_LOCAL_NED)) {
+            set_position_local_support_confirmed = true;
+            ROS_INFO_NAMED("setpoint_raw", "SPR: Set position target local command is supported.");
+        } else {
+            set_position_local_support_confirmed = false;
+            ROS_WARN_NAMED("setpoint_raw",
+                           "SPR: Set position target local command is not supported.");
+        }
 
-        set_position_target_local_ned(req->header.stamp.toNSec() / 1000000, req->coordinate_frame,
-                                      req->type_mask, position, velocity, af, yaw, yaw_rate);
+        if (m_uas->has_capability(UAS::MAV_CAP::SET_POSITION_TARGET_GLOBAL_INT)) {
+            set_position_global_support_confirmed = true;
+            ROS_INFO_NAMED("setpoint_raw", "SPR: Set position target global command is supported.");
+        } else {
+            set_position_global_support_confirmed = false;
+            ROS_WARN_NAMED("setpoint_raw",
+                           "SPR: Set position target global command is not supported.");
+        }
+    }
+
+    /* ros callbacks */
+    /* ROS回调函数 */
+
+    void local_cb(const mavros_msgs::PositionTarget::ConstPtr &req) {
+        if (set_position_local_support_confirmed) {
+            Eigen::Vector3d position, velocity, af;
+            float           yaw, yaw_rate;
+
+            tf::pointMsgToEigen(req->position, position);
+            tf::vectorMsgToEigen(req->velocity, velocity);
+            tf::vectorMsgToEigen(req->acceleration_or_force, af);
+
+            // Transform frame ENU->NED
+            // 坐标系从ENU转到NED
+            if (req->coordinate_frame == mavros_msgs::PositionTarget::FRAME_BODY_NED ||
+                req->coordinate_frame == mavros_msgs::PositionTarget::FRAME_BODY_OFFSET_NED) {
+                position = ftf::transform_frame_baselink_aircraft(position);
+                velocity = ftf::transform_frame_baselink_aircraft(velocity);
+                af       = ftf::transform_frame_baselink_aircraft(af);
+                yaw      = ftf::quaternion_get_yaw(
+                         ftf::transform_orientation_absolute_frame_baselink_aircraft(
+                             ftf::quaternion_from_rpy(0.0, 0.0, req->yaw)));
+            } else {
+                position = ftf::transform_frame_enu_ned(position);
+                velocity = ftf::transform_frame_enu_ned(velocity);
+                af       = ftf::transform_frame_enu_ned(af);
+                yaw      = ftf::quaternion_get_yaw(
+                         ftf::transform_orientation_baselink_aircraft(ftf::transform_orientation_enu_ned(
+                             ftf::quaternion_from_rpy(0.0, 0.0, req->yaw))));
+            }
+
+            Eigen::Vector3d ang_vel_enu(0.0, 0.0, req->yaw_rate);
+            auto            ang_vel_ned = ftf::transform_frame_enu_ned(ang_vel_enu);
+            yaw_rate                    = ang_vel_ned.z();
+
+            set_position_target_local_ned(req->header.stamp.toNSec() / 1000000,
+                                          req->coordinate_frame, req->type_mask, position, velocity,
+                                          af, yaw, yaw_rate);
+        } else {
+            ROS_WARN_NAMED("setpoint_raw",
+                           "SPR: Operation error, please check capabilities of FCU!");
+        }
     }
 
     void global_cb(const mavros_msgs::GlobalPositionTarget::ConstPtr &req) {
-        Eigen::Vector3d velocity, af;
-        float           yaw, yaw_rate;
+        if (set_position_global_support_confirmed) {
+            Eigen::Vector3d velocity, af;
+            float           yaw, yaw_rate;
 
-        tf::vectorMsgToEigen(req->velocity, velocity);
-        tf::vectorMsgToEigen(req->acceleration_or_force, af);
+            tf::vectorMsgToEigen(req->velocity, velocity);
+            tf::vectorMsgToEigen(req->acceleration_or_force, af);
 
-        // Transform frame ENU->NED
-        velocity = ftf::transform_frame_enu_ned(velocity);
-        af       = ftf::transform_frame_enu_ned(af);
-        yaw      = ftf::quaternion_get_yaw(ftf::transform_orientation_aircraft_baselink(
-            ftf::transform_orientation_ned_enu(ftf::quaternion_from_rpy(0.0, 0.0, req->yaw))));
-        Eigen::Vector3d ang_vel_enu(0.0, 0.0, req->yaw_rate);
-        auto            ang_vel_ned = ftf::transform_frame_ned_enu(ang_vel_enu);
-        yaw_rate                    = ang_vel_ned.z();
+            // Transform frame ENU->NED
+            // 坐标系从ENU转到NED
+            velocity = ftf::transform_frame_enu_ned(velocity);
+            af       = ftf::transform_frame_enu_ned(af);
+            yaw      = ftf::quaternion_get_yaw(ftf::transform_orientation_baselink_aircraft(
+                     ftf::transform_orientation_enu_ned(ftf::quaternion_from_rpy(0.0, 0.0, req->yaw))));
+            Eigen::Vector3d ang_vel_enu(0.0, 0.0, req->yaw_rate);
+            auto            ang_vel_ned = ftf::transform_frame_enu_ned(ang_vel_enu);
+            yaw_rate                    = ang_vel_ned.z();
 
-        set_position_target_global_int(req->header.stamp.toNSec() / 1000000, req->coordinate_frame,
-                                       req->type_mask, req->latitude * 1e7, req->longitude * 1e7,
-                                       req->altitude, velocity, af, yaw, yaw_rate);
+            set_position_target_global_int(req->header.stamp.toNSec() / 1000000,
+                                           req->coordinate_frame, req->type_mask,
+                                           req->latitude * 1e7, req->longitude * 1e7, req->altitude,
+                                           velocity, af, yaw, yaw_rate);
+        } else {
+            ROS_WARN_NAMED("setpoint_raw",
+                           "SPR: Operation error, Please check capabilities of FCU!");
+        }
     }
 
     void attitude_cb(const mavros_msgs::AttitudeTarget::ConstPtr &req) {
-        Eigen::Quaterniond desired_orientation;
-        Eigen::Vector3d    baselink_angular_rate;
-        Eigen::Vector3d    body_rate;
-        double             thrust;
+        if (set_attitude_support_confirmed) {
+            Eigen::Quaterniond desired_orientation;
+            Eigen::Vector3d    baselink_angular_rate;
+            Eigen::Vector3d    body_rate;
+            double             thrust;
 
-        // ignore thrust is false by default, unless no thrust scaling is set or thrust is zero
-        auto ignore_thrust = req->thrust != 0.0 && thrust_scaling < 0.0;
+            // ignore thrust is false by default, unless no thrust scaling is set or thrust is zero
+            // ignore thrust变量缺省值为false，除非没有设置拉力比例或比例为0
+            auto ignore_thrust = req->thrust != 0.0 && thrust_scaling < 0.0;
 
-        if (ignore_thrust) {
-            // I believe it's safer without sending zero thrust, but actually ignoring the
-            // actuation.
-            ROS_FATAL_THROTTLE_NAMED(5, "setpoint_raw",
-                                     "Recieved thrust, but ignore_thrust is true: "
-                                     "the most likely cause of this is a failure to specify the "
-                                     "thrust_scaling parameters "
-                                     "on px4/apm_config.yaml. Actuation will be ignored.");
-            return;
-        } else {
-            if (thrust_scaling == 0.0) {
-                ROS_WARN_THROTTLE_NAMED(5, "setpoint_raw",
-                                        "thrust_scaling parameter is set to zero.");
+            if (ignore_thrust) {
+                ROS_ERROR_NAMED(
+                    "setpoint_raw",
+                    "SPR: Recieved thrust, but ignore_thrust is true: the most likely cause of "
+                    "this is a failure to specify the thrust_scaling parameters on "
+                    "px4/apm_config.yaml. Actuation will be ignored.");
+                return;
+            } else {
+                if (thrust_scaling == 0.0) {
+                    ROS_WARN_NAMED("setpoint_raw", "thrust_scaling parameter is set to zero.");
+                }
+                thrust = std::min(1.0, std::max(0.0, req->thrust * thrust_scaling));
             }
-            thrust = std::min(1.0, std::max(0.0, req->thrust * thrust_scaling));
+
+            // Take care of attitude setpoint
+            // 注意角度设置
+            desired_orientation = ftf::to_eigen(req->orientation);
+
+            // Transform desired orientation to represent aircraft->NED,
+            // MAVROS operates on orientation of base_link->ENU
+            // 将期望旋转从FLU->ENU转到FRD->NED
+            auto ned_desired_orientation = ftf::transform_orientation_enu_ned(
+                ftf::transform_orientation_baselink_aircraft(desired_orientation));
+
+            body_rate = ftf::transform_frame_baselink_aircraft(ftf::to_eigen(req->body_rate));
+
+            set_attitude_target(req->header.stamp.toNSec() / 1000000, req->type_mask,
+                                ned_desired_orientation, body_rate, thrust);
+        } else {
+            ROS_WARN_NAMED("setpoint_raw",
+                           "SPR: Operation error, Please check capabilities of FCU!");
         }
-
-        // Take care of attitude setpoint
-        desired_orientation = ftf::to_eigen(req->orientation);
-
-        // Transform desired orientation to represent aircraft->NED,
-        // MAVROS operates on orientation of base_link->ENU
-        auto ned_desired_orientation = ftf::transform_orientation_enu_ned(
-            ftf::transform_orientation_baselink_aircraft(desired_orientation));
-
-        body_rate = ftf::transform_frame_baselink_aircraft(ftf::to_eigen(req->body_rate));
-
-        set_attitude_target(req->header.stamp.toNSec() / 1000000, req->type_mask,
-                            ned_desired_orientation, body_rate, thrust);
     }
 };
 } // namespace std_plugins
